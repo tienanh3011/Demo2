5068bf7d85dc9e80fd80fa87ea1c811bfafe9a59
tienanh3011 <106862092+tienanh3011@users.noreply.github.com>
2022-07-31 17:17:42 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,300 +1,137 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Threading;
+using UnityEngine;
 
-namespace Testing
+public class Character : MonoBehaviour
 {
-    class Test
+    //private int i = 0;
+    public float speed = 10f;
+    public float speedjump = 10f;
+    private bool isJump;
+    private bool isFall;
+    
+    //di chuyen tu do hinh chu nhat
+    //Vector2 target;
+    //private bool isPos0, isPos1, isPos2, isPos3;
+    //public Transform pos0;
+    //public Transform pos1;
+    //public Transform pos2;
+    //public Transform pos3;
+    // Start is called before the first frame update
+    void Start()
     {
-        private static int x = 0;
-        private static int y = 0;
-        private static ArrayList m1 = new ArrayList();
-        static void Main(string[] args)
+        //Debug.Log("======== Ham Start");
+        //isPos0 = false;
+        //isPos1 = true;
+        //isPos2 = false;
+        //isPos3 = false;
+        isFall = false;
+        isJump = false;
+        transform.position = new Vector2(-12.7f, -4.66f);
+    }
+    //Time.delta la thoi gian giua 2 frame
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.Translate(Vector2.left * speed * Time.deltaTime);
+        //transform.Translate(Vector2.up * speed * Time.deltaTime);
+        //transform.Translate(Vector2.right * speed * Time.deltaTime);
+        //transform.Translate(Vector2.down * speed * Time.deltaTime);
+
+
+        //Vector2 _pos = transform.localScale;
+        //float step = 1.0f;
+        //di chuyen phim mui ten
+        if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            Reprint();
-
-            var readKeys = new Task(ReadKeys);
-            readKeys.Start();
-
-            var tasks = new[] { readKeys };
-
-            Task.WaitAll(tasks);
-
-            Console.CancelKeyPress += (sender, e) =>
-            {
-                Environment.Exit(0);
-            };
-
+            
+            isJump = true;
         }
-        private static void MenuOrginal()
+        if (isJump)
         {
-            Console.Clear();
-            Console.WriteLine("+---------+---------+");
-            Console.WriteLine("|        Menu:      |");
-            Console.WriteLine("+---------+---------+");
-            Console.BackgroundColor = ConsoleColor.Red;
-            Console.WriteLine("1.Nhap lai day so");
-            Console.BackgroundColor = ConsoleColor.White;
-            Console.WriteLine("2.Sap xep tang dan");
-            Console.WriteLine("3.Sap xep giam dan");
-            Console.WriteLine("4.In ket qua");
-            Console.WriteLine("5.Demo sx tang dan");
-            Console.WriteLine("6.Thoat");
-            Console.WriteLine("+---------+---------+");
+            transform.Translate(Vector2.up * speedjump * Time.deltaTime);
         }
-        private static void Menu()
+        if (transform.position.y>=0)
         {
-            Console.SetCursorPosition(x, y);
-            Console.WriteLine("+---------+---------+");
-            Console.WriteLine("|        Menu:      |");
-            Console.WriteLine("+---------+---------+");
-            Console.WriteLine("1.Nhap lai day so");
-            Console.WriteLine("2.Sap xep tang dan");
-            Console.WriteLine("3.Sap xep giam dan");
-            Console.WriteLine("4.In ket qua");
-            Console.WriteLine("5.Demo sx tang dan");
-            Console.WriteLine("6.Thoat");
-            Console.WriteLine("+---------+---------+");
+
+            isFall = true;
+            isJump = false;
         }
-        private static int Y = 3;
-        private static void ReadKeys()
+        if(isFall)
         {
-            int Order;
-            ConsoleKeyInfo key = new ConsoleKeyInfo();
-            while (!Console.KeyAvailable && key.Key != ConsoleKey.Escape)
-            {
-                key = Console.ReadKey(true);
+            transform.Translate(Vector2.down * speedjump * Time.deltaTime);
+        }
+        if (transform.position.y < -4.66f)
+        {
 
-                switch (key.Key)
-                {
-                    case ConsoleKey.UpArrow:
-                        if (Y != 3)
-                        {
-                            switch (Y)
-                            {
-                                case 4:
-                                    Menu();
-                                    Y--;
-                                    UpDown(Y);
-                                    break;
-                                case 5:
-                                    Menu();
-                                    Y--;
-                                    UpDown(Y);
-                                    break;
-                                case 6:
-                                    Menu();
-                                    Y--;
-                                    UpDown(Y);
-                                    break;
-                                case 7:
-                                    Menu();
-                                    Y--;
-                                    UpDown(Y);
-                                    break;
-                                case 8:
-                                    Menu();
-                                    Y--;
-                                    UpDown(Y);
-                                    break;
-                            }
-                        }
-                        break;
+            transform.position = new Vector2(transform.position.x, -4.66f);
+            isFall = false;
+        }
+        //if (Input.GetKeyDown(KeyCode.DownArrow))
+        //{
+        //    transform.Translate(Vector2.down * speed * Time.deltaTime);
 
-                    case ConsoleKey.DownArrow:
-                        if (Y != 8)
-                        {
-                            switch (Y)
-                            {
-                                case 3:
-                                    Menu();
-                                    Y++;
-                                    UpDown(Y);
-                                    break;
-                                case 4:
-                                    Menu();
-                                    Y++;
-                                    UpDown(Y);
-                                    break;
-                                case 5:
-                                    Menu();
-                                    Y++;
-                                    UpDown(Y);
-                                    break;
-                                case 6:
-                                    Menu();
-                                    Y++;
-                                    UpDown(Y);
-                                    break;
-                                case 7:
-                                    Menu();
-                                    Y++;
-                                    UpDown(Y);
-                                    break;
-                            }
-                        }
-                        break;
-
-                    case ConsoleKey.Enter:
-                        switch (Y)
-                        {
-                            case 3:
-                                Reprint();
-                                break;
-                            case 4:
-                                Order = 0;
-                                Sort(Order);
-                                Console.SetCursorPosition(x + 25, Y);
-                                Console.Write("Da xep sep xong, bam phim bat ky de tiep tuc");
-                                Console.ReadLine();
-                                Console.SetCursorPosition(x + 25, Y);
-                                Console.ForegroundColor = ConsoleColor.White;
-                                Console.Write("Da xep sep xong, bam phim bat ky ");
-                                Console.ForegroundColor = ConsoleColor.Black;
-                                break;
-                            case 5:
-                                Order = 1;
-                                Sort(Order);
-                                Console.SetCursorPosition(x + 25, Y);
-                                Console.Write("Da xep sep xong, bam phim bat ky");
-                                Console.ReadLine();
-                                Console.SetCursorPosition(x + 25, Y);
-                                Console.ForegroundColor = ConsoleColor.White;
-                                Console.Write("Da xep sep xong, bam phim bat ky");
-                                Console.ForegroundColor = ConsoleColor.Black;
-                                break;
-                            case 6:
-                                Print();
-                                break;
-                            case 7:
-
-                                break;
-                            case 8:
-                                Environment.Exit(0);
-                                break;
-                        }
-                        break;
-                }
-            }
+        //}
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
         }
 
-        private static void UpDown(int Y)
-        {
-            Console.SetCursorPosition(x, Y);
-            Console.BackgroundColor = ConsoleColor.Red;
-            switch(Y)
-            {
-                case 3:
-                    Console.WriteLine("1.Nhap lai day so");
-                    break;
-                case 4:
-                    Console.WriteLine("2.Sap xep tang dan");
-                    break;
-                case 5:
-                    Console.WriteLine("3.Sap xep giam dan");
-                    break;
-                case 6:
-                    Console.WriteLine("4.In ket qua");
-                    break;
-                case 7:
-                    Console.WriteLine("5.Demo sx tang dan");
-                    break;
-                case 8:
-                    Console.WriteLine("6.Thoat");
-                    break;
-            }
-            Console.BackgroundColor = ConsoleColor.White;
-        }
+        //_pos.y-=step
+        //di chuyen tu do hinh chu nhat
+        //  if(isPos1)
+        //  {
+        //      target = pos1.position;
+        //       if(Vector2.Distance(transform.position,pos1.position)<0.1f)
+        //      {
+        //          isPos0 = false;
+        //          isPos1 = false;
+        //          isPos2 = true;
+        //          isPos3 = false;
+        //      }
 
-        private static void Reprint()
-        {
-            Console.CursorVisible = true;
-            Console.BackgroundColor = ConsoleColor.White;
-            Console.ForegroundColor = ConsoleColor.Black;
-            Console.Clear();
+        //  }
+        //  if (isPos2)
+        //  {
+        //      target = pos2.position;
+        //       if (Vector2.Distance(transform.position, pos2.position) < 0.1f)
+        //      {
+        //          isPos0 = false;
+        //          isPos1 = false;
+        //          isPos2 = false;
+        //          isPos3 = true;
+        //      }
 
-            int n = 7;
-            m1.Clear();
-            for (int i = 0; i < n; i++)
-            {
-                Console.Write("Nhap vao so {0} cua day so cua ban: ", i);
-                int t = int.Parse(Console.ReadLine());
-                m1.Add(t);
-            }
+        //  }
+        //  if (isPos3)
+        //  {
+        //      target = pos3.position;
+        //       if (Vector2.Distance(transform.position, pos3.position) < 0.1f)
+        //      {
+        //          isPos0 = true;
+        //          isPos1 = false;
+        //          isPos2 = false;
+        //          isPos3 = false;
+        //      }
 
-            for (int i = 3; i >= 0; i--)
-            {
-                Console.SetCursorPosition(x, y + (n));
-                Console.WriteLine("Screen will clear in {0}", i);
-                Thread.Sleep(1000);
-            }
-            Console.CursorVisible = false;
+        //  }
+        //  if (isPos0)
+        //  {
+        //      target = pos0.position;
+        //       if (Vector2.Distance(transform.position, pos0.position) < 0.1f)
+        //      {
+        //          isPos0 = false;
+        //          isPos1 = true;
+        //          isPos2 = false;
+        //          isPos3 = false;
+        //      }
 
-            MenuOrginal();
-        }
+        //  }
+        //transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
 
-        private static int[] sorted = new int[12];
-        private static void Sort(int Order)
-        {
-            Array.Clear(sorted, 0, sorted.Length);
-
-            for (int i = 0; i < m1.Count; i++)
-            {
-                sorted[i] = Convert.ToInt32(m1[i]);
-            }
-            if (Order == 0)
-            {
-                for (int i = 0; i < m1.Count; i++)
-                {
-                    for (int j = i; j < m1.Count; j++)
-                    {
-                        if (sorted[i] > sorted[j])
-                        {
-                            int tmp = sorted[i];
-
-                            sorted[i] = sorted[j];
-
-                            sorted[j] = tmp;
-                        }
-                    }
-                }
-            }
-            else if (Order == 1)
-            {
-                for (int i = 0; i < m1.Count; i++)
-                {
-                    for (int j = i; j < m1.Count; j++)
-                    {
-                        if (sorted[i] < sorted[j])
-                        {
-                            int tmp = sorted[i];
-
-                            sorted[i] = sorted[j];
-
-                            sorted[j] = tmp;
-                        }
-                    }
-                }
-            }  
-        }
-        private static void Print()
-        {
-            int plus = 0;
-            for (int i = 0; i < m1.Count; i++)
-            {
-                Console.SetCursorPosition(x + 20 + plus +i, Y);
-                Console.WriteLine("+----+");
-                Console.SetCursorPosition(x + 20 + plus + i, Y + 1);
-                Console.WriteLine(sorted[i] + " | ");
-                Console.SetCursorPosition(x + 20 + plus + i, Y + 2);
-                Console.WriteLine("+----+");
-                plus 
-            }
-        }
     }
 }
-
